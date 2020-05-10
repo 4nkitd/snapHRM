a3b31ec567c7a6aadec3b660318ca8bd6372906a
Ankit <4nkitd@gmail.com>
2020-05-10 15:22:11 +0530

    api scripts added

--- README.md
+++ README.md
@@ -5,4 +5,20 @@
 > # snaphrm api's
 > since snaphrm api's were not public i had to extract them fron the website. and used it to ferform the same task and much more
 
-# under Active Dev.
+--
+
+> # api_attandance_marker_clock_in.py
+>define below vars to use this script
+>host = "example"
+>email="example@example.com"
+>password="example"
+
+--
+
+> # api_attandance_marker_clock_in.py
+>define below vars to use this script
+>host = "example"
+>email="example@example.com"
+>password="example"
+
+# Under Active Development.
---
+++ api_attandance_marker_clock_out.py
@@ -0,0 +1,45 @@
+# importing the requests library 
+import requests 
+
+# api-endpoint 
+host = "example"
+email="example@example.com"
+password="example"
+
+def authToken(host,email,password):
+	api_url = 'https://'+host+'.snaphrm.com/api/v1/auth/login'
+
+	body = {"email":email,"password":password,"remember_me":"false"}
+ 
+	headers = {'X-SNAPHRM-HOST':host+'.snaphrm.com'}
+	r = requests.post(url = api_url, params = body,headers=headers) 
+
+	data = r.json() 
+
+
+	print(data['message'])
+	print('') 
+	return data['data']['token']
+
+def clock_in(host,token):
+	api_url = 'https://'+host+'.snaphrm.com/api/v1/attendance/clock-out'
+
+	body = {}
+ 
+	headers = {'X-SNAPHRM-HOST':host+'.snaphrm.com','Authorization':'Bearer '+token}
+	r = requests.post(url = api_url, params = body,headers=headers) 
+
+	data = r.json() 
+
+
+	print(data)
+	print('') 
+
+
+print('Auto Clock-Out SnapHRM') 
+print('by github.com/4nkitd') 
+print('') 
+
+token = authToken(host,email,password)
+
+clock_in(host,token)
